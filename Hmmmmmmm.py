a683a38414aff6beeea5bc182c96a7f792eea013
JackLR7392 <156823085+JackLR7392@users.noreply.github.com>
2026-01-23 23:56:42 +0800

    Add files via upload

---
+++ Hmmmmmmm.py
@@ -0,0 +1,30 @@
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("white")
+wn.title("hmmmmmm")
+
+t = turtle.Turtle()
+t.pu
+t.goto(200, 30)
+t.setheading(135)
+t.clear
+t.pensize(10)
+t.pencolor("Blue")
+t.pd()
+t.forward(100)
+t.right(-90)
+t.forward(200)
+t.right(90)
+t.forward(100)
+t.pu()
+t.goto(50, 170)
+t.setheading(-135)
+t.pd()
+t.forward(100)
+t.right(-90)
+t.forward(200)
+t.right(90)
+t.forward(100)
+t.pu()
+
